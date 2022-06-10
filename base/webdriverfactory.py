c17c6feed78534adb9d03c3eb30ccaf67455fa7c
juannaman <juan.naman@totalperform.com>
2022-06-10 09:05:45 -0300

    Anda todo joya

--- base/webdriverfactory.py
+++ base/webdriverfactory.py
@@ -1,19 +1,17 @@
 from selenium import webdriver
 
+
 class WebDriverFactory():
 
     def __init__(self, browser):
-
         self.browser = browser
+
     def getWebDriverInstance(self):
 
-
-        if self.browser == "iexplorer":
-            driver = webdriver.Ie()
-        elif self.browser == "firefox":
-            driver = webdriver.Firefox()
+        if self.browser == "firefox":
+            driver = webdriver.Firefox(executable_path='../drivers/geckodriver.exe',  service_log_path=None)
         elif self.browser == "chrome":
-            driver = webdriver.Chrome()
+            driver = webdriver.Chrome(executable_path=r'../drivers/chromedriver.exe')
         else:
             driver = webdriver.Firefox()
         driver.implicitly_wait(10)
