765c7aa218341a739c965cace7df35c9b28ef109
juannaman <juan.naman@totalperform.com>
2022-06-10 09:30:25 -0300

    Anda todo con reportes por consola

--- tests/test_Execution.py
+++ tests/test_Execution.py
@@ -1,21 +1,5 @@
 import pytest
-# """FORMA 1 DE PARAMETRIZAR"""
-# @pytest.fixture(params=["a", "b"])    #Le paso parametros a un fixture.  CORRE LA FIXTURE CON A, EL TEST, LUEGO LA FIXTURE CON B Y EL TEST. ITERA USANDO CADA VEZ UN PARAMETRO
-# def demo_fixture(request):            #Le paso request para acceder a los parametros
-#     print(request.param)              #Le paso request.param para acceder a los parametros.
-#
-#
-# def test_login(demo_fixture):
-#     print('Login succesfull')
-
-
-
-
 import unittest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import time
-import HtmlTestRunner
 from selenium.webdriver.common.by import By
 from page_objects.youtube_main import YoutubeMainPage
 from page_objects.google_main import GoogleMainPage
@@ -28,19 +12,12 @@ class TestingDEPT(unittest.TestCase):
 
     @pytest.fixture(autouse=True)
     def classSetup(self, oneTimeSetUp):
-        # chrome_option = Options()
-        # chrome_option.add_argument('start-maximized')
-        # chrome_option.add_argument('incognito')
-        # # self.driver = webdriver.Chrome(options=chrome_option)
-        # #self.driver = webdriver.Firefox()
-        # self.driver = webdriver.Edge(executable_path=r'C:\Users\naman.MSI\Documents\workspace_python\DEPT\drivers\msedgedriver.exe')
-        #self.driver.implicitly_wait(10)
         self.youtubemainpage = YoutubeMainPage(self.driver)
         self.googlemainpage = GoogleMainPage(self.driver)
         self.facebookmainpage = FacebookMainPage(self.driver)
         self.facebooksingup = FacebookSingUp(self.driver)
 
-    #@unittest.skip
+    @pytest.mark.run(order=1)
     def test_youtube(self):
         youtube_url = 'https://www.youtube.com/'
         google_url = 'https://www.google.com/'
@@ -62,19 +39,5 @@ class TestingDEPT(unittest.TestCase):
         last_name_alert_present = self.facebooksingup.first_name_alert_present()
         self.assertTrue(last_name_alert_present)
 
-
-
-
-
-        time.sleep(5)
-
-
-    # def tearDown(self):
-    #     self.driver.close()
-    #     self.driver.quit()
-
-
-if __name__ == '__main__':
-    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='reporte.html'))
 
 
